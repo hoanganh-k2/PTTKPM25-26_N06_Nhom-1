bce261a670d1c25f86d0958aa9b161ba859ad8e8
Hoang Anh <anhh3642@gmail.com>
2025-09-11 14:14:00 +0700

    Update route admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,12 @@ import { AuthProvider } from "./contexts/AuthContext";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import AdminLayout from "./layouts/AdminLayout";
+import DashboardPage from "./pages/admin/DashboardPage";
+import BooksManagementPage from "./pages/admin/BooksManagementPage";
+import BookFormPage from "./pages/admin/BookFormPage";
+import OrdersManagementPage from "./pages/admin/OrdersManagementPage";
+import UsersManagementPage from "./pages/admin/UsersManagementPage";
 import "./App.css";
 
 function App() {
@@ -10,10 +16,20 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
+          {/* Public Routes */}
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          {/* Các routes khác sẽ được thêm sau */}
+          
+          {/* Admin Routes */}
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<DashboardPage />} />
+            <Route path="books" element={<BooksManagementPage />} />
+            <Route path="books/add" element={<BookFormPage />} />
+            <Route path="books/edit/:id" element={<BookFormPage />} />
+            <Route path="orders" element={<OrdersManagementPage />} />
+            <Route path="users" element={<UsersManagementPage />} />
+          </Route>
         </Routes>
       </Router>
     </AuthProvider>
